8323f79f96cca103af01640d809cb55055fe7605
yogesh sharma <80456879+yogeshsharma27@users.noreply.github.com>
2023-08-24 13:59:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   version= "5.11.0"
+}
+ } 
 }
 
+provider "aws" {
+  region = "ap-south-1 "
+}
 resource "aws_instance" "web" {
 
   ami           = "ami-0da59f1af71ea4ad2"
