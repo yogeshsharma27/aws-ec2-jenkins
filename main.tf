96e56223ec4a2a0a5d6ac9b11991911d0d9ddebf
yogesh sharma <80456879+yogeshsharma27@users.noreply.github.com>
2023-08-25 13:39:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "web" {
 
-  ami           = "ami-0da59f1af71ea4ad2"
+  ami           = "ami-0453898e98046c639"
   instance_type = "t2.micro"
-
-  subnet_id     = "subnet-07c83fc3bd10d7f51"
-
-  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "HelloWorld"
